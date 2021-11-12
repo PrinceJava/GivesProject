8420963b8e38c7f3840e973bc3eee3723ae32c48
Raul Rosales <raurosales@paypal.com>
2021-11-12 11:10:49 -0700

    built out role class

--- src/main/java/com/javaproject/javaprojectthree/model/Role.java
+++ src/main/java/com/javaproject/javaprojectthree/model/Role.java
@@ -1,5 +1,33 @@
 package com.javaproject.javaprojectthree.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
 public class Role {
-    //this is a comment
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Role() {}
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
 }
+
