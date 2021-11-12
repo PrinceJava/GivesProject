d375024bfe05363a76bbb16485f61ab69227d944
matthewljames <matjames@paypal.com>
2021-11-12 12:02:21 -0600

    added JWT, JWTUtils, MyUserDetails, MyUserDetailsService, and Security Config

---
+++ src/main/java/com/javaproject/javaprojectthree/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.javaproject.javaprojectthree.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        if(user == null){
+            throw new UsernameNotFoundException("Username not found in database");
+        }
+        return new MyUserDetails(user);
+    }
+}
