01cb381bf6efd33db29ef7954554cf7de33bad04
matthewljames <matjames@paypal.com>
2021-11-14 11:14:14 -0600

    completed first DAO to front end URL people.html

--- src/main/java/com/javaproject/javaprojectthree/controller/UserController.java
+++ src/main/java/com/javaproject/javaprojectthree/controller/UserController.java
@@ -2,6 +2,7 @@ package com.javaproject.javaprojectthree.controller;
 import com.javaproject.javaprojectthree.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
 
