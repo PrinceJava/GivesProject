f37213f8cb8f6c4ba640979c8bfecc139b82ec85
matthewljames <matjames@paypal.com>
2021-11-14 10:54:16 -0600

    people.html with thymeleaf table

--- src/main/java/com/javaproject/javaprojectthree/controller/UserController.java
+++ src/main/java/com/javaproject/javaprojectthree/controller/UserController.java
@@ -2,6 +2,8 @@ package com.javaproject.javaprojectthree.controller;
 import com.javaproject.javaprojectthree.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,10 @@ public class UserController {
     @Autowired
     public void setUserService(UserService userService){this.userService = userService;}
 
+    @GetMapping("/all")
+    public String findAllUsers(Model model){
+        model.addAttribute("listUsers",userService.findAllUsers());
+        return "people";
+    }
 
 }
