4eb893cec62ed1593b1cff3caf06d387d9ad6901
matthewljames <matjames@paypal.com>
2021-11-15 15:54:19 -0600

    framework for JWT login

--- src/main/java/com/javaproject/javaprojectthree/controller/AuthenticationController.java
+++ src/main/java/com/javaproject/javaprojectthree/controller/AuthenticationController.java
@@ -7,12 +7,10 @@ import com.javaproject.javaprojectthree.model.forms.RegisterForm;
 import com.javaproject.javaprojectthree.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class AuthenticationController {
 
@@ -20,6 +18,11 @@ public class AuthenticationController {
 
     @Autowired
     public void setUserService(UserService userService){this.userService = userService;}
+
+    @GetMapping("/login")
+    public String userLogin(){
+        return "login";
+    }
 
     @PostMapping("/register")
     public User createUser(@RequestBody RegisterForm registerForm){
--- src/main/java/com/javaproject/javaprojectthree/controller/CharityController.java
+++ src/main/java/com/javaproject/javaprojectthree/controller/CharityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,4 +37,11 @@ public class CharityController {
         return "charities";
     }
 
+    @GetMapping("/{charityId}")
+    public String findCharityById(
+            @PathVariable(value = "charityId") Long charityId,
+            Model model){
+        model.addAttribute("charity",charityService.findCharityById(charityId));
+        return "charity";
+    }
 }
